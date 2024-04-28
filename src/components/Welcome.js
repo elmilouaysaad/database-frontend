ad12ba3ecbee3c2d7dcd1d0a3127aac477f961ed
saad <proesaad@gmail.com>
2024-04-28 20:24:41 +0100

    a

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -41,11 +41,14 @@ export function Welcome({loginClient,signUp}) {
                     <li>Visit us at: 123 Main Street, Anytown, USA</li>
                 </ul>
             </div>
-            
-      <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800"><span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 
+    
         AUI || Made for database Class by Saad Elmilouay, Younes Mellouk, Ahmed Taha Baitou, Omar Lhoumadi
     </span>
         
+        </div>
         </footer>
         </div>
     );
