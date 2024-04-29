de75f0fde3ae22e2c9accc7410840b877ab89c4e
saad <proesaad@gmail.com>
2024-04-29 14:24:33 +0100

    added crediantials1

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -124,23 +124,15 @@ export function AdminDashboard() {
                   </p>
                   <Link
                     to={"/"}
-                    className="inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold text-white transition hover:bg-white hover:text-primary"
                   >
-                    Go To Home
+                    <button className="inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold text-white transition hover:bg-white hover:text-primary">
+                    Go To Home</button>
                   </Link>
                 </div>
               </div>
             </div>
           </div>
   
-          <div className="absolute left-0 top-0 flex h-full w-full items-center justify-between space-x-5 md:space-x-8 lg:space-x-14">
-            <div className="h-full w-1/3 bg-gradient-to-t "></div>
-            <div className="flex h-full w-1/3">
-              <div className="h-full w-1/2 bg-gradient-to-b "></div>
-              <div className="h-full w-1/2 bg-gradient-to-t "></div>
-            </div>
-            <div className="h-full w-1/3 bg-gradient-to-b "></div>
-          </div>
         </div>
   )
     }
