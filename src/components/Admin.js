7e0276790cb6ffa4553f9c57e179ae68096cc25b
saad <proesaad@gmail.com>
2024-04-29 14:39:17 +0100

    added crediantials

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -109,9 +109,12 @@ export function AdminDashboard() {
     );}
     else{
         return( 
-        <div>
-         <span>401</span> 
-        </div>
+            <div class="min-h-screen flex flex-grow items-center justify-center bg-gray-50">
+            <div class="rounded-lg bg-white p-8 text-center shadow-xl">
+              <h1 class="mb-4 text-4xl font-bold">401</h1>
+              <p class="text-gray-600">Oops! The page you are looking for has special crediantials.</p>
+            </div>
+          </div>
   )
     }
 }
